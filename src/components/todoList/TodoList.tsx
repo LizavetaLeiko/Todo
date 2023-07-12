9427e8a97bef0b3a751d9219374481e522751836
LizavetaLeiko <lizaleiko3@gmail.com>
2023-07-12 15:33:57 +0300

    added 0 todo message

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,12 +1,13 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import Todo from "../todo/Todo";
 import { IToDo } from "../../interfaces/ITodo";
 import { useAppSelector } from "../../store/reduxHook";
 import theme from "../../theme";
 
 const TodoList = () => {
-
-  const todos: IToDo[] = useAppSelector((state) =>[...state].reverse());
+  const todos: IToDo[] = useAppSelector((state) =>
+    [...state].reverse()
+  );
 
   return (
     <Flex
@@ -20,11 +21,31 @@ const TodoList = () => {
       bg={theme.colors.white}
       gap={2}
     >
-      {todos.map((item: IToDo) => {
-        return (
-          <Todo key={item.id} task={item.task} id={item.id} isDone={item.isDone}/>
-        );
-      })}
+      {todos.length > 0 ? (
+        todos.map((item: IToDo) => {
+          return (
+            <Todo
+              key={item.id}
+              task={item.task}
+              id={item.id}
+              isDone={item.isDone}
+            />
+          );
+        })
+      ) : (
+        <Text
+          marginBottom={2}
+          textAlign={"center"}
+          color={theme.colors.purple}
+          fontSize={{
+            base: "24px",
+            md: "32px",
+            sm: "28px",
+          }}
+        >
+          You don't have any tasks yet
+        </Text>
+      )}
     </Flex>
   );
 };
